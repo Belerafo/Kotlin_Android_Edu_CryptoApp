1cce5a2f8d96baa58188b55a7df37e8d7373ccf2
Belerafo <68774802+Belerafo@users.noreply.github.com>
2021-02-24 08:24:03 +0300

    24.02.2021

--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.cryptoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cryptoapp.api.apiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
 
 class MainActivity : AppCompatActivity() {
+
+    private  val  compositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val disposable = apiFactory.apiService.getTopCoinInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+               // TextViewCenter.setText(it.toString())
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            },{
+                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
+            })
+            compositeDisposable.add(disposable)
+
     }
 }
+
+
+
